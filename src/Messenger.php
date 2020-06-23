b67fcfa75b4a09608d25a9a3fe6ed453de360afa
SylveK <s.tobys@gmail.com>
2020-06-23 12:38:51 +0200

    wrong namespacing

--- src/Messenger.php
+++ src/Messenger.php
@@ -2,152 +2,88 @@
 
 namespace SylveK\LaravelMessenger;
 
+use SylveK\LaravelMessenger\Models\Thread;
+use SylveK\LaravelMessenger\Models\Message;
+use SylveK\LaravelMessenger\Models\Participant;
+
 class Messenger
 {
-    /**
-     * Map for the messenger's models.
-     *
-     * @var array
-     */
+    // -- Map for the messenger's models.
     protected $models = [];
 
-    /**
-     * Map for the messenger's tables.
-     *
-     * @var array
-     */
+    // -- Map for the messenger's tables.
     protected $tables = [];
 
-    /**
-     * Internal pointer name for the app's "user" model.
-     *
-     * @var string
-     */
+    // -- Internal pointer name for the app's "user" model.
     private $userModelLookupKey = 'User';
 
-    /**
-     * Set the model to be used for threads.
-     *
-     * @param string $model
-     */
+    // -- Set the model to be used for threads.
     public function setMessageModel($model)
     {
         $this -> models[Message::class] = $model;
     }
 
-    /**
-     * Set the model to be used for participants.
-     *
-     * @param  string $model
-     */
+    // -- Set the model to be used for participants.
     public function setParticipantModel($model)
     {
         $this -> models[Participant::class] = $model;
     }
 
-    /**
-     * Set the model to be used for threads.
-     *
-     * @param  string $model
-     */
+    // -- Set the model to be used for threads.
     public function setThreadModel($model)
     {
         $this -> models[Thread::class] = $model;
     }
 
-    /**
-     * Set the model to be used for users.
-     *
-     * @param  string  $model
-     */
+    // -- Set the model to be used for users.
     public function setUserModel($model)
     {
         $this -> models[$this -> userModelLookupKey] = $model;
     }
 
-    /**
-     * Set custom table names.
-     *
-     * @param  array $map
-     */
+    // -- Set custom table names.
     public function setTables(array $map)
     {
         $this -> tables = array_merge($this -> tables, $map);
     }
 
-    /**
-     * Get a custom table name mapping for the given table.
-     *
-     * @param  string $table
-     * @return string
-     */
+    // -- Get a custom table name mapping for the given table.
     public function table($table)
     {
         return $this -> tables[$table] ?? $table;
     }
 
-    /**
-     * Get the class name mapping for the given model.
-     *
-     * @param  string $model
-     * @return string
-     */
+    // -- Get the class name mapping for the given model.
     public function classname($model)
     {
         return $this -> models[$model] ?? $model;
     }
 
-    /**
-     * Get an instance of the messages model.
-     *
-     * @param  array $attributes
-     * @return \Cmgmyr\Messenger\Models\Message
-     */
+    // -- Get an instance of the messages model.
     public function message(array $attributes = [])
     {
         return $this -> make(Message::class, $attributes);
     }
 
-    /**
-     * Get an instance of the participants model.
-     *
-     * @param  array $attributes
-     * @return \Cmgmyr\Messenger\Models\Participant
-     */
+    // -- Get an instance of the participants model.
     public function participant(array $attributes = [])
     {
         return $this -> make(Participant::class, $attributes);
     }
 
-    /**
-     * Get an instance of the threads model.
-     *
-     * @param  array $attributes
-     * @return \Cmgmyr\Messenger\Models\Thread
-     */
+    // -- Get an instance of the threads model.
     public function thread(array $attributes = [])
     {
         return $this -> make(Thread::class, $attributes);
     }
 
-    /**
-     * Get an instance of the user model.
-     *
-     * @param  array  $attributes
-     * @return \Illuminate\Database\Eloquent\Model
-     */
+    // -- Get an instance of the user model.
     public function user(array $attributes = [])
     {
         return $this -> make($this -> userModelLookupKey, $attributes);
     }
 
-    /**
-     * Get an instance of the given model.
-     *
-     * @param  string $model
-     * @param  array $attributes
-     * @return \Illuminate\Database\Eloquent\Model
-     */
+    // -- Get an instance of the given model.
     protected function make($model, array $attributes = [])
     {
         $model = $this -> classname($model);
